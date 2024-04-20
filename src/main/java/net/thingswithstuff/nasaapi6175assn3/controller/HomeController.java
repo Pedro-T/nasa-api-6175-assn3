8d79c6ada57ed0a3d328b9faa28708ff5a5be96f
Pedro <Pedro-T@users.noreply.github.com>
2024-04-20 07:17:09 -0400

    fix error page when user attempts to access sub-pages directly

--- src/main/java/net/thingswithstuff/nasaapi6175assn3/controller/HomeController.java
+++ src/main/java/net/thingswithstuff/nasaapi6175assn3/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/current", "/random", "/date-single", "/date-range"})
     public String index() {
         return "index";
     }
